e80b27397929144403edff88775dcd795c2b6e3a
rafal <rafal@DESKTOP-NB5L087>
2018-09-27 14:33:16 +0200

    update

--- F1_DMA_USART_CMSIS/inc/my_lib_usart.h
+++ F1_DMA_USART_CMSIS/inc/my_lib_usart.h
@@ -9,15 +9,15 @@
 #ifndef MY_LIB_USART_H_
 #define MY_LIB_USART_H_
 
-#define RX_BUFFER_SIZE 256
-
-
+#define RX_BUFFER_SIZE 16
+#define RX_END_OF_LINE "\r"
+//#define RX_END_OF_LINE "\l\n"
 
 void __usart_tx_init(void);
 void __dma_transmit_init(char *buffer);
 void __usart_rx_init(void);
 void __usart_rx_irq_enable(void);
-uint32_t __usart_rx_check_line_ended(void);
+uint32_t __usart_rx_get_line(char *buffer);
 
 
 #endif /* MY_LIB_USART_H_ */
